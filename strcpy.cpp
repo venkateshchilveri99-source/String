c1819ae1b3ac8d3e0fac3c525eb9b68142afd8ec
Venkatesh Chilveri <venkateshchilveri99@gmail.com>
2026-07-26 20:38:38 +0530

    Add files via upload

---
+++ strcpy.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str1[6]="Hello";
+    char str2[10];
+
+strcpy(str2,str1);
+cout<<"Copied string = "<<str2;
+
+    
+return 0;
+}
