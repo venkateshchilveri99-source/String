723f2664017204a62ecb755d6750c2c83fb3fc2c
Venkatesh Chilveri <venkateshchilveri99@gmail.com>
2026-07-27 20:32:30 +0530

    Add files via upload

---
+++ strcat.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str1[6]="Hello",str2[7]="World";
+strcat(str1,str2);
+cout<<"Concatted string= :"<<str1;
+
+return 0;
+}
