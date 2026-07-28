ebd9c2bd9f3abacd198054c6cbe18d1b15be4709
Venkatesh Chilveri <venkateshchilveri99@gmail.com>
2026-07-28 08:48:55 +0530

    Add files via upload

---
+++ strcmp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str1[6]="Hello",str2[7]="Hello";
+if(strcmp(str1,str2)==0)
+{
+    cout<<"Two strings are Equal";
+}
+else
+cout<<"Two strings are not Equal";
+
+    
+return 0;
+}
