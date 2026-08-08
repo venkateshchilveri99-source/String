0a1a16dcfaf3da2831d7d81d65ab00be43d4bf73
Venkatesh Chilveri <venkateshchilveri99@gmail.com>
2026-08-08 21:17:08 +0530

    Create 2dstring.cpp

---
+++ 2dstring.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char names[3][20] = {
+        "Venkatesh",
+        "Rahul",
+        "Amit"
+    };
+
+    for (int i = 0; i < 3; i++) {
+        printf("%s\n", names[i]);
+    }
+
+    return 0;
+}
